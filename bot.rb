ecb7c0ac398a3b0831f160f0a56465c77c6f6a41
rocery <sastranuralamsyah@gmail.com>
2020-12-01 12:14:28 +0700

    4

--- bot.rb
+++ bot.rb
@@ -10,24 +10,51 @@ bot.get_updates(fail_silently: true) do |message|
       case command
         # /start
         when /start/i
-          reply.text = "All I can do is say hello. Try the /greet and /download command."
-        # /greet
-        when /greet/i
-          reply.text = "Hello, #{message.from.first_name}. 🤖"
-        when /download/i
           reply.text = "
-          Link Download kebutuhan opersional TH : /link_download_it 
-          \n WARP 1.1.1. : /warp
-          \n IT & Trainer : /itrainer
+          Selamat datang di BOT Helper ID Express Mitra Bandung.
+          Kamu mencari solusi disini melalui command yang ada, yaitu:
+          Info Trainer dan IT    : /infoTrainer
+          Link Download Software : /downloadSoftware
+          Alur Scan dll          : /alur
           "
-        when /link_download_it/i
-          reply.text = "bit.ly/itjabar atau s.id/itjabar"
-        when /warp/i
-          reply.text = "1.1.1.1/beta"
-        when /itrainer/i
+        when /infoTrainer/i
           reply.text = "
-            Sastra : @Sastra_ITrainer
-          "   
+          Ardhy Head Trainer : @Ardhy_ITrainer
+          Dian : @Dian_ITrainer
+          Hilmy : @Hilmy_ITranier
+          Ryfan : @Ryfan_ITrainer
+          Sastra : @Sastra_ITrainer
+          "
+        when /downloadSoftware/i
+          reply.text = "
+          Software Operasional : s.id/itjabar atau bit.ly/itjabar
+          Warp : 1.1.1.1/beta/
+          "
+        when /alur/i
+          reply.text = "
+          Retur : /retur
+          "
+        when /retur/i
+          reply.text = "
+          Confirm retur : Ya tinggal Confirm xD
+          "
+          # # /greet
+        # when /greet/i
+        #   reply.text = "Hello, #{message.from.first_name}. 🤖"
+        # when /download/i
+        #   reply.text = "
+        #   Link Download kebutuhan opersional TH : /link_download_it 
+        #   \n WARP 1.1.1. : /warp
+        #   \n IT & Trainer : /itrainer
+        #   "
+        # when /link_download_it/i
+        #   reply.text = "bit.ly/itjabar atau s.id/itjabar"
+        # when /warp/i
+        #   reply.text = "1.1.1.1/beta"
+        # when /itrainer/i
+        #   reply.text = "
+        #     Sastra : @Sastra_ITrainer
+        #   "   
       end
       puts "sending #{reply.text.inspect} to @#{message.from.username}"
       reply.send_with(bot)
